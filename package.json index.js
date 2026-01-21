fad1fa1bc9f099386675e5e714428813b7c13f78
jeeetu78-afk <jeeetu78@gmail.com>
2026-01-21 23:57:04 +0530

    Create package.json index.js

---
+++ package.json index.js
@@ -0,0 +1,13 @@
+{
+  "name": "desi-ai-backend",
+  "version": "1.0.0",
+  "description": "Simple Desi AI backend",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "cors": "^2.8.5"
+  }
+}
